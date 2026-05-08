3ad7ef173b09cd81b2885b647b8d02d362f69477
Isac O. <ornelasisac13@gmail.com>
2026-05-07 22:34:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "admin.html"));
+});
+
+app.get("/offline", (req, res) => {
+  res.sendFile(path.join(__dirname, "offline.html"));
+});
+
+app.get("/maintenance", (req, res) => {
+  res.sendFile(path.join(__dirname, "maintenance.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
